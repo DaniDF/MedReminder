f68056eeb3fd1e5172d6d025460f5eb7d21d7165
dani <danieledraghi2@gmail.com>
2022-10-20 13:01:33 +0200

    Added new sample view

---
+++ app/src/main/java/it/dani/medreminder/view/add/NewSampleActivity.kt
@@ -0,0 +1,12 @@
+package it.dani.medreminder.view.add
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import it.dani.medreminder.R
+
+class NewSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.activity_new_sample)
+    }
+}
